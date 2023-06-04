f69f3ecb1bdc045252a927575d8644107339310f
Rishav9123105662 <96661505+Rishav9123105662@users.noreply.github.com>
2023-06-04 22:21:52 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,22 @@
+gsap.from(".a",{
+    stagger: 0.1,
+    duration: 2,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    x: -8,
+});
+
+gsap.from(".b",{
+    stagger: 0.1,
+    duration: 2,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    x: -5,
+});
+gsap.from(".c",{
+    stagger: 0.1,
+    duration: 2,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    y: -30,
+});
